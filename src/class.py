8ccca80b1b74511b93dde1807db9468218a81649
Adam Grigolato <adam.grigolato@balcora.net>
2016-07-11 01:32:35 +1000

    commiting inprogress code before signing off

---
+++ src/class.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+###
+###
+###
+Author = 'Adam Grigolato'
+Version = '0'
+###
+###
+###
+import json
+import yaml
+
+class pupclass(object):
+    def __init__( self, type, repo, name, json = "" ):
+        self.name = name
+        self.type = type
+        self.file = "{}/heiradata/{}.{}".format(repo,name,type)
+        self.json = json
+
+
+    def add(self):
+        if self.type = "yaml":
+            with open(self.file, 'w') as outfile:
+                yaml.dump(json.loads(self.json), outfile, default_flow_style=True)
+        elif self.type = "json":
+            with open(self.file, 'w') as outfile:
+                json.dump(self.json, outfile, indent=4)
+            
+            
+
+
+    def delete(self):
+
+
+    def debug(self):
+        print(self.name)
+        print(self.type)
+        print(self.file)
+        print(self.json)
+
+
+test = pupclass("json", "/home/cora/.pupcon/repos/C0R3/", "test")
+test.debug()
+
+    
